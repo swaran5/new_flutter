4fdcd18bc6a9900c64aaadfef2e467d87140c338
swaran5 <73553856+swaran5@users.noreply.github.com>
2021-06-22 16:23:56 +0530

    pagination added

--- lib/DataFromApi.dart
+++ lib/DataFromApi.dart
@@ -1,6 +1,5 @@
 
 import 'dart:convert';
-import 'dart:ffi';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,13 +13,52 @@ class DataFromApi extends StatefulWidget{
 }
 
 class _DataFromAPIStake extends State{
+  List<Datum> users = [];
+  int pagenum = 1;
+  ScrollController scrollController = ScrollController();
 
-  Future<Users> getUserData() async {
-    var response = await http.get(Uri.https("reqres.in", "/api/users", {"page" : "1"}));
+  Future<List<Datum>> getUserData() async {
+    var response = await http.get(Uri.https("reqres.in", "/api/users", {"page" : pagenum.toString()}));
     var jsonData = jsonDecode(response.body);
 
-    final Users users = usersFromJson(response.body);
+    setState(() {
+      users = usersFromJson(response.body).data;
+    });
     return users;
+  }
+
+  getMoreData() async {
+    print("get more users");
+    pagenum = pagenum+1;
+    var response = await http.get(Uri.https("reqres.in", "/api/users", {"page" : pagenum.toString()}));
+
+    if(usersFromJson(response.body).data != null) {
+      setState(() {
+        users.addAll(usersFromJson(response.body).data);
+      });
+      return users;
+    }
+    else{
+      return;
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    getUserData();
+
+    scrollController.addListener(() {
+      double maxScroll = scrollController.position.maxScrollExtent;
+      double currentScroll = scrollController.position.pixels;
+      double delta = MediaQuery.of(context).size.height * 0.2;
+
+      if(maxScroll - currentScroll <= delta){
+        getMoreData();
+      }
+    });
   }
 
   @override
@@ -32,33 +70,41 @@ class _DataFromAPIStake extends State{
   }
 
   Widget getListView() {
-   return FutureBuilder(
-        future: getUserData(),
-        builder: (BuildContext context,AsyncSnapshot snapshot){
-          if (snapshot.connectionState == ConnectionState.done) {
-            if (snapshot.data == null) {
-              return Text("null");
-            }
-            else {
-              ListView listView = ListView.builder(
-                  itemCount: snapshot.data.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return getItemView(snapshot, index);
-                  });
-              return listView;
-            }
-          }
-          else if (snapshot.connectionState == ConnectionState.waiting) {
-            return Text('Error'); // error
-          }
-          else {
-            return CircularProgressIndicator(); // loading
-          }
-        }
-    );
+   return ListView.builder(
+       itemCount: users.length,
+       controller: scrollController,
+       itemBuilder: (BuildContext context, int index) {
+         return getItemView(index);
+       });
+
+    //  FutureBuilder(
+    //     future: getUserData(),
+    //     builder: (BuildContext context,AsyncSnapshot snapshot){
+    //       if (snapshot.connectionState == ConnectionState.done) {
+    //         if (snapshot.data == null) {
+    //           return Text("null");
+    //         }
+    //         else {
+    //           ListView listView = ListView.builder(
+    //               itemCount: snapshot.data.length,
+    //               controller: scrollController,
+    //               itemBuilder: (BuildContext context, int index) {
+    //                 return getItemView(snapshot, index);
+    //               });
+    //           return listView;
+    //         }
+    //       }
+    //       else if (snapshot.connectionState == ConnectionState.waiting) {
+    //         return Text('Error'); // error
+    //       }
+    //       else {
+    //         return CircularProgressIndicator(); // loading
+    //       }
+    //     }
+    // );
   }
 
-  Widget getItemView(AsyncSnapshot snapshot, int index) {
+  Widget getItemView(int index) {
     Widget itemView = Card(
         shape:
         RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
@@ -69,7 +115,7 @@ class _DataFromAPIStake extends State{
             Expanded(
               flex: 3,
               child: Container(
-                height: 120,
+                height: 130,
                 child: ClipRRect(
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20.0),
@@ -80,7 +126,7 @@ class _DataFromAPIStake extends State{
                   child: Image(
                     fit: BoxFit.fill,
                     alignment: Alignment.centerLeft,
-                    image: NetworkImage(snapshot.data.data[index].avatar),
+                    image: NetworkImage(users[index].avatar),
                   ),
                 ),
               ),
@@ -96,20 +142,20 @@ class _DataFromAPIStake extends State{
 
                       Container(
                           width: double.infinity,
-                          height: 60,
+                          height: 65,
                           child: Align(
                               alignment: Alignment.centerLeft,
                               child: Text(
-                                "First Name :${snapshot.data.data[index].firstName}",
+                                "First Name :${users[index].firstName}",
                                 style: TextStyle(color: Colors.black, fontSize: 25),
                               ))),
                       Container(
                           width: double.infinity,
-                          height: 60,
+                          height: 65,
                           child: Align(
                               alignment: Alignment.topLeft,
                               child: Text(
-                                "Last Name :${snapshot.data.data[index].lastName}",
+                                "Last Name :${users[index].lastName}",
                                 style: TextStyle(color: Colors.black, fontSize: 25),
                               ))),
                     ],
